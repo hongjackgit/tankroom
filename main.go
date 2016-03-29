4af7d1f4aa2819af2d98b5a69a88f758ce97f3d9
hongjackgit <493755152@qq.com>
2016-03-29 19:47:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import(
-  "fmt"
+import (
+    "tank"
+    "fmt"
+    "os"
+  //  "reflect"
 )
 
 func main(){
-  fmt.Println("tankroom will be start")
+    
+     
+    if(len(os.Args) == 1){
+        fmt.Println("params is empty")
+
+        /*tankClient := &tank.TankClient{make(chan string),"client1"}
+        var roomMap = map[string] *tank.TankClient{"client1":tankClient}
+        tankRooms := &tank.TankRoom{"title",roomMap}
+        fmt.Println(reflect.TypeOf(tankRooms))
+        tankRooms.Test()
+*/
+    } else if(os.Args[1] == "s"){
+        tankService := tank.CreateTankService("127.0.0.1:9996")
+        tankService.Start()
+    } else if(os.Args[1] == "c"){
+        tankClient :=tank.CreateClient()
+        tankClient.Start()
+        
+    }
+    
 }
